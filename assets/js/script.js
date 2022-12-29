3f5f921125407bf9a4be80219dda0b60c9957c46
Artur Bomtempo <arturbcolen@gmail.com>
2022-12-29 19:46:20 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ function switchTheme() {
     document.body.classList.toggle('light-theme');
 
     const theme = document.body.classList[0];
-    const music = theme === 'light-theme' ? 'normal-world.mpeg' : 'inverted-world.mpeg'
+    const music = theme === 'light-theme' ? 'normal-world.mpeg' : 'inverted-world.mpeg';
 
     const audio = document.getElementById('music');
     audio.src = `./assets/musics/${music}`;
