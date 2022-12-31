d36dd6e96ed6ffc65941ba3f1d60d08c47b22690
Artur Bomtempo Colen <arturbcolen@gmail.com>
2022-12-31 16:29:00 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-// Function to change the themes of each world
+// Change the theme of each world
 function switchTheme() {
     document.body.classList.toggle('dark-theme');
     document.body.classList.toggle('light-theme');
