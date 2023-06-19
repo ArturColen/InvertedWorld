f6241993843a50b8d5d163110764e5362c02db78
Artur Bomtempo Colen <arturbcolen@gmail.com>
2023-06-19 09:38:03 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,5 @@
-// Import script from database
 import { getHellfireClubSubscriptions, subscribeToHellfireClub } from './firebase/hellfire-club.js';
 
-// Create constants for each form field
 const txtName = document.getElementById('txtName');
 const txtEmail = document.getElementById('txtEmail');
 const txtLevel = document.getElementById('txtLevel');
@@ -16,10 +14,8 @@ btnSubscribe.addEventListener('click', async () => {
         character: txtCharacter.value
     };
 
-    // Save data in the database
     const subscriptionId = await subscribeToHellfireClub(subscription);
 
-    // Clear form fields
     txtName.value = '';
     txtEmail.value = '';
     txtLevel.value = '';
